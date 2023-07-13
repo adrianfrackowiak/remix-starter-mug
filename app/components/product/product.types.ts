5f27e9d9f60dd12cf9a765c44d5bbcded94b4a97
Adrian Frąckowiak <adrian.frackowiak@mug.pl>
2023-07-13 13:07:31 +0200

    wip

---
+++ app/components/product/product.types.ts
@@ -0,0 +1,6 @@
+export interface ProductProps {
+  title: string;
+  description: string;
+  image: string;
+  price: number;
+}
---
+++ app/interfaces/api/index.ts
@@ -0,0 +1,1 @@
+export type { ProductSettings } from './product.interface';
