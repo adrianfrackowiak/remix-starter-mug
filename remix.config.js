4b40941523ee9f4065c71860444716aaeaf35583
Adrian Frąckowiak <adrian.frackowiak@mug.pl>
2023-07-14 07:21:17 +0200

    wip

--- remix.config.js
+++ remix.config.js
@@ -1,10 +1,23 @@
+const { withEsbuildOverride } = require("remix-esbuild-override");
+const styledComponentsPlugin = require("./styled-components-esbuild-plugin");
+
+withEsbuildOverride((option) => {
+  option.plugins.unshift(styledComponentsPlugin());
+
+  return option;
+});
+
+
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
-  serverDependenciesToBundle: ['swiper', 'swiper/react', 'swiper/css'],
+  serverDependenciesToBundle: ['swiper', 'swiper/react', 'swiper/css', 'styled-components'],
   server: process.env.NODE_ENV === "development" ? undefined : "./server.ts",
-  serverBuildPath: "api/index.js",
   serverModuleFormat: "cjs",
+  appDirectory: "app",
+  assetsBuildDirectory: "public/build",
+  serverBuildPath: "build/index.js",
+  publicPath: "/build/",
   future: {
     v2_dev: true,
     v2_errorBoundary: true,
